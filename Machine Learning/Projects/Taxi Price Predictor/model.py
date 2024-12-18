62fbf3adbe7106c2a1e55ddda01fa5757222098b
animesh7780 <thakuranimesh235@gmail.com>
2024-12-18 08:22:00 +0530

    Update model.py

--- Machine Learning/Projects/Taxi Price Predictor/model.py
+++ Machine Learning/Projects/Taxi Price Predictor/model.py
@@ -7,8 +7,16 @@ from sklearn.impute import SimpleImputer
 # Load dataset
 data = pd.read_csv('taxi_trip_pricing.csv')
 
+#replace categorical values with numerical
+data.replace({'Time_of_Day': {'Morning': 0, 'Afternoon': 1, 'Evening': 2, 'Night': 3},
+    'Day_of_Week': {'Weekday': 0, 'Weekend': 1},
+    'Traffic_Conditions': {'Low': 0, 'Medium': 1, 'High': 2},
+    'Weather': {'Clear': 0, 'Cloudy': 1, 'Rain': 2, 'Snow': 3}
+    }, inplace=True)
+
+
 # Select features and target
-features = ['Trip_Distance_km', 'Passenger_Count', 'Base_Fare', 'Per_Km_Rate', 'Per_Minute_Rate', 'Trip_Duration_Minutes']
+features = ['Trip_Distance_km', 'Time_of_Day', 'Day_of_Week', 'Passenger_Count', 'Traffic_Conditions', 'Weather', 'Base_Fare', 'Per_Km_Rate', 'Per_Minute_Rate', 'Trip_Duration_Minutes']
 target = ['Trip_Price']
 
 # Split data into training and testing sets
@@ -32,7 +40,7 @@ model.fit(X_train, Y_train)
 Y_predicted = model.predict(X_test)
 
 # Custom input values for prediction
-custom_input = np.array([[19.35, 3, 3.56, 0.8, 0.32, 53.82]])  
+custom_input = np.array([[19.35,0,0,3.0,0,0,3.56,0.8,0.32,53.82]])  
 
 print("Custom input shape:", custom_input.shape) 
 
